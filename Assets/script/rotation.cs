9d9681f5577e4e9c0df404eb8b2d81ef4852f050
ak47sototan <ak47sototan@gmail.com>
2021-03-08 13:16:08 +0900

    バグ修正1

---
+++ Assets/script/rotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        float x = 0.2f;
+        this.transform.Rotate(0, x, 0);
+    }
+}
