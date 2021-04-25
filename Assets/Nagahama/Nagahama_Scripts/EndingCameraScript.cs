698c0e0efaebc53b44e79c4872010adfd4c03a7c
ak47sototan <ak47sototan@gmail.com>
2021-04-25 10:31:24 +0900

    エンディング演出作成ー途中

---
+++ Assets/Nagahama/Nagahama_Scripts/EndingCameraScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCameraScript : MonoBehaviour
+{
+    public bool rotflg = false;
+    public Transform target;
+    public float speed = 2f;
+    float step;
+
+    void Start()
+    {
+        StartCoroutine(nameof(TimeScaleDown));
+    }
+
+    void Update()
+    {
+        if (rotflg) {
+            if(target == null) {
+                rotflg = false;
+                return;
+            }
+            Vector3 targetDir = target.position - transform.position;
+            targetDir.y = transform.position.y; //targetと高さが異なると体ごと上下を向いてしまうので制御
+            float step = speed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 10.0F);
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+    }
+
+    private IEnumerator TimeScaleDown()
+    {
+        yield return new WaitForSeconds(1.75f);
+        Time.timeScale = 0.2f;
+        rotflg = true;
+        yield return new WaitForSeconds(2 * 0.2f);
+        Time.timeScale = 1f;
+    }
+}
---
+++ Assets/Nagahama/Nagahama_Scripts/EndingPlayerScript.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingPlayerScript : MonoBehaviour
+{
+    [System.Serializable]
+    public class MissileShotSettings
+    {
+
+        [SerializeField, Tooltip("ミサイルの発射時初速")]
+        public float[] _missileShotPower;
+
+        [SerializeField, Tooltip("次回発射までのクールタイム")]
+        public float _missileShotDelay;
+
+        [SerializeField, Tooltip("複数ターゲット発射時、次ターゲット発射までの待機時間")]
+        public float _multiTargetMissileDelay;
+
+        [SerializeField, Tooltip("アニメーショントリガーの名前")]
+        public string _animationTriggerName;
+
+        [SerializeField, Tooltip("アニメーションの長さ")]
+        public float _animWaitTime;
+
+        [SerializeField, Tooltip("ミサイルの発射方向")]
+        public Transform[] _halfwayPoints;
+
+        [SerializeField, Tooltip("着弾までの時間")]
+        public float[] _impactTimes;
+
+        [SerializeField, Tooltip("発射してから生成までの待機時間")]
+        public float[] _instantiateTimes;
+    }
+
+    [SerializeField] private EndingCameraScript _endCam;
+
+    [Header("サウンド系")]
+    [SerializeField] private SoundPlayer _soundPlayer;
+    [SerializeField] private AudioClip _se_MissileLaunch;
+
+    [Header("ロベリア")]
+    [SerializeField] private Transform _target;
+
+    [Header("ミサイルの設定"), Space(10)]
+    [SerializeField] private Transform _launchPoint;
+    [SerializeField] private GameObject _missilePrefab;
+    [SerializeField] private int _missileDamage = 1;
+
+    [Header("下段にそのグループ内の何発目のミサイルに追従するか指定できる")]
+    [Header("上段に何回目に発射されたミサイルのグループにキルカメラが追従するか")]
+    [SerializeField, Tooltip("何回目に生成されたミサイルのグループにカメラが追従するか")] private int _followKillCamMultiTargetMissilesCount;
+    [SerializeField, Tooltip("キルカメラ時何番目に生成されたミサイルをカメラが追従するか")] private int _followKillCamMissileNum;
+
+    [SerializeField, Space(10)] private MissileShotSettings _missileShotSettings;
+
+
+    void Start()
+    {
+        StartCoroutine(nameof(MultiTargetMissileInstantiate));
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator MultiTargetMissileInstantiate()
+    {
+        yield return new WaitForSeconds(1.5f);
+        int i = 0;
+        foreach (var hp in _missileShotSettings._halfwayPoints) {
+            
+            StartCoroutine(MissileInstantiate(i++));
+        }
+
+    }
+
+    private IEnumerator MissileInstantiate(int index)
+    {
+        yield return new WaitForSeconds(_missileShotSettings._instantiateTimes[index]);
+        GameObject missile = MissileShot(_missileShotSettings._halfwayPoints[index], _missileShotSettings._impactTimes[index], index);
+
+    }
+
+    /// <summary>
+    /// 複数呼び出すときのターゲット指定版
+    /// </summary>
+    /// <param name="target">ターゲットのTransform</param>
+    /// <param name="halfwaypoint">ミサイルが一度通過する中間地点</param>
+    /// <param name="impacttime">着弾するまでの時間</param>
+    private GameObject MissileShot(Transform halfwaypoint, float impacttime, int index)
+    {
+        GameObject missile = Instantiate(_missilePrefab, _launchPoint.position, Quaternion.identity);
+        HomingMissileScript homingMissileScript = missile.GetComponent<HomingMissileScript>();
+
+        homingMissileScript.LaunchMissile(_target, halfwaypoint, impacttime, _launchPoint.position - halfwaypoint.position, _missileShotSettings._missileShotPower[index], _missileDamage);
+
+        _soundPlayer.PlaySE(_se_MissileLaunch);
+
+        if(index == 0) {
+            _endCam.target = missile.transform;
+            _endCam.rotflg = true;
+        }
+
+        return missile;
+    }
+}
