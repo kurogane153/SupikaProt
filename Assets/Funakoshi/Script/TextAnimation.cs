939c5897596cdc5433568b1461286d24a4b27fbf
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2021-04-27 16:14:29 +0900

    タイトルのアニメーションの追加（ロゴのアニメーション、ロゴが出た後に出る光のアニメーション、Start Option Endボタンのアニメーション）

---
+++ Assets/Funakoshi/Script/TextAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextAnimation : MonoBehaviour
+{
+    
+    public float speed = 0.01f;  //透明化の速さ
+    float alfa;    //A値を操作するための変数
+    float red, green, blue;    //RGBを操作するための変数
+
+    void Start()
+    {
+        
+
+        //Panelの色を取得
+        red = GetComponent<Text>().color.r;
+        green = GetComponent<Text>().color.g;
+        blue = GetComponent<Text>().color.b;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().color = new Color(red, green, blue, alfa);
+        alfa += speed;
+    }
+}
