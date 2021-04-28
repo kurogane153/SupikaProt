77e7f8c765f285472c54457412058fe56adf054d
hayato nagahama <11807078@stu.kbc.ac.jp>
2021-04-28 10:24:40 +0900

    ミニマップ実装

---
+++ Assets/Nagahama/Nagahama_Scripts/MinimapObjectScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapObjectScript : MonoBehaviour
+{
+    [SerializeField] private Color[] _colors;
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void ChangeColor(int arrayNumber)
+    {
+        if(_colors.Length <= arrayNumber) {
+            Debug.LogError(gameObject.name + "の" + nameof(ChangeColor) + "に配列要素数を超えた値が指定されました");
+            return;
+        }
+
+        meshRenderer.material.color = _colors[arrayNumber];
+    }
+
+}
