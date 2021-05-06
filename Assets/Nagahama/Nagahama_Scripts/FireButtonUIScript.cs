73391a4d6fd997c252003b472b35a797e87d9c4d
hayato nagahama <11807078@stu.kbc.ac.jp>
2021-05-06 14:00:37 +0900

    1個以上隕石をロックオンしているとき「B　発射」UIが出るようにした

---
+++ Assets/Nagahama/Nagahama_Scripts/FireButtonUIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FireButtonUIScript : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    [SerializeField] private TextMeshProUGUI _text;
+
+    // レティクルのカウント数を参照し、1個でもロックオンしていれば発射ボタンUIをアクティブにする
+    void Update()
+    {
+        if (ReticleController.Instance.GeneratedReticleCount >= 1) {
+            UIElementSetActive(true);
+        } else {
+            UIElementSetActive(false);
+        }
+    }
+
+    private void UIElementSetActive(bool flag)
+    {
+        _image.enabled = flag;
+        _text.enabled = flag;
+    }
+}
